c9e889f81e3b4b8f72b7b91d36460049c94bb346
小田崇文 <oda-takafumi@odatakafuminoMacBook-Pro.local>
2021-09-12 04:37:30 +0900

    CircleCI用にCapistranoの設定を修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,2 @@
-set :ssh_options, {
-  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
-  forward_agent: true,
-  auth_methods: %w[publickey]
-}
+server '18.177.206.247', user: 'oda', roles: %w{app db web}
+set :ssh_options, keys: '~/.ssh/koumuin_next_aws_rsa'
