8837bc933f17cf3a57f6dc6d06f1e48d9e781ca7
小田崇文 <oda-takafumi@odatakafuminoMacBook-Pro.local>
2021-09-12 05:55:11 +0900

    statusカラム及びゲストユーザーの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,26 @@
 Age.create([{category: '10代'},{category: '20代'},{category: '30代'},{category: '40代'},{category: '50代以上'}])
 Organization.create([{category: '国家公務員'},{category: '地方公務員'}])
 Job.create([{category: '事務系'},{category: '技術系'}])
+Status.create([{category: '現職'},{category: '退職済'}])
 
-User.create(
+User.create([{
   name: '管理者ユーザー',
   email: 'admin@email',
-  password: 'pass',
-  password_confirmation: 'pass',
+  password: 'admin',
+  password_confirmation: 'admin',
   age_id: 3,
   organization_id: 2,
   job_id: 1,
-  admin: true
-)
+  status_id: 1,
+  admin: true},
+{
+  name: 'ゲストユーザー',
+  email: 'guest@email',
+  password: 'guest',
+  password_confirmation: 'guest',
+  age_id: 2,
+  organization_id: 1,
+  job_id: 1,
+  status_id: 2,
+  admin: false
+}])
