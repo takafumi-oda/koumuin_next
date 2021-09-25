27f5e4acc3d42cf05ece7bfc5ce78f48f793e62a
小田崇文 <datti_1988@icloud.com>
2021-09-26 05:06:54 +0900

    いいね数のカウントを追加

--- app/javascript/bundles/Like.jsx
+++ app/javascript/bundles/Like.jsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { HeartFill } from 'react-bootstrap-icons';
 import axios from 'axios';
@@ -6,26 +5,32 @@ import axios from 'axios';
 axios.defaults.headers['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
 const Like = (props) => {
-  const { replyId, likeId } = props;
+  const { replyId, likeId, likeCount, user } = props;
   const [id, setId] = useState(likeId);
+  const [count, setCount] = useState(likeCount);
 
   const clicked = async () => {
-    if (id == null) {
-      const { data: { id = null } } = await axios.post('/likes', { reply_id: replyId })
-      id && setId(id);
+    if (user == null) {
+      alert('いいね機能はログイン後のみ使用可能です');
     } else {
-      await axios.delete(`/likes/${id}`)
-      setId(null);
+      if (id == null) {
+        const { data: { id = null } } = await axios.post('/likes', { reply_id: replyId })
+        id && setId(id);
+        setCount(count+1);
+      } else {
+        await axios.delete(`/likes/${id}`)
+        setId(null);
+        setCount(count-1);
+      }
     }
   }
 
   return (
     <>
-      <HeartFill color={id ? 'red' : 'gray'} size={32} onClick={clicked}/>
+      <HeartFill color={id? 'red' : 'gray'} size={24} onClick={clicked}/>
+      {count}
     </>
   );
 };
-
-Like.propTypes = {};
 
 export default props => <Like {...props} />;
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -7,6 +7,6 @@ class Reply < ApplicationRecord
   validates :post_id, presence: true
 
   def like_by(user)
-    likes.find{|like| like.user_id == user.id}
+    likes.find { |like| like.user_id == user.id } if user
   end
 end
