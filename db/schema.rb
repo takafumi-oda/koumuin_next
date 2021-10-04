963e8531b86a3dd76e075b390472d6d06b02adb6
小田崇文 <datti_1988@icloud.com>
2021-10-04 17:12:57 +0900

    Userテーブルに備考（管理用）を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_194820) do
+ActiveRecord::Schema.define(version: 2021_10_04_081114) do
 
   create_table "favorites", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2021_10_03_194820) do
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "note"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
