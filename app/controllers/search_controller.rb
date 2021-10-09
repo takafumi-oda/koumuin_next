07d5065533ad082514eff415b7f40f0911c7fc65
小田崇文 <datti_1988@icloud.com>
2021-10-10 06:35:05 +0900

    N+1問題の解消

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def search
-    @posts = Post.search(params[:keyword])
+    @posts = Post.search(params[:keyword]).includes([:user])
     @replies = Reply.search(params[:keyword])
     search_object = @posts + Post.where(id: @replies.select(:post_id).distinct)
     @search_posts = search_object.uniq
