69e7ea5ab5d1b84c1c15e69ad00e96ec10e03fb5
小田崇文 <datti_1988@icloud.com>
2021-10-12 04:10:01 +0900

    ユーザー情報関連のレイアウト修正

--- app/javascript/bundles/Modal.jsx
+++ app/javascript/bundles/Modal.jsx
@@ -10,13 +10,36 @@ const Modal = (props) => {
       {props.showFlag ? (
         <div className = "overlay">
           <div className = "modal-window">
-            <p>{props.user.name}</p>
-            <p>{props.user.age}</p>
-            <p>{props.user.organization}</p>
-            <p>{props.user.job}</p>
-            <p>{props.user.status}</p>
-            <p>{props.user.introduction}</p>
-            <button onClick={closeModal}>Close</button>
+            <p className = "card-header">ユーザー情報</p>
+            <div className = "card-body">
+              <div className = "card-box">
+                <p className = "card-name">ユーザー名</p>
+                <p className = "card-text">{props.user.name}</p>
+              </div>
+              <div className = "card-box">
+                <p className = "card-name">年代</p>
+                <p>{props.user.age}</p>
+              </div>
+              <div className = "card-box">
+                <p className = "card-name">組織</p>
+                <p>{props.user.organization}</p>
+              </div>
+              <div className = "card-box">
+                <p className = "card-name">職種</p>
+                <p>{props.user.job}</p>
+              </div>
+              <div className = "card-box">
+                <p className = "card-name">現在の状況</p>
+                <p>{props.user.status}</p>
+              </div>
+              <div className = "card-box">
+                <p className = "card-name">自己紹介</p>
+                <p>{props.user.introduction}</p>
+              </div>
+              <div className = "btn-position">
+                <button className = "close-button" onClick={closeModal}>Close</button>
+              </div>
+            </div>
           </div>
         </div>
       ) : (
