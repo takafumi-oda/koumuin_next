991875db636e553a3a850e3002619d10dca63ec2
小田崇文 <datti_1988@icloud.com>
2021-10-18 15:28:14 +0900

    検索機能の修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,17 @@
 class SearchController < ApplicationController
   def search
-    @search_posts = Post.all.includes([:user, :replies]) if params[:keyword].blank?
-    keywords = params[:keyword].split(/[[:blank:]]+/).select(&:present?)
-    keywords.each do |keyword|
-      @posts = Post.search(keyword).includes([:user, :replies])
-      @replies = Reply.search(keyword)
-      search_object = @posts + Post.where(id: @replies.select(:post_id).distinct).includes([:user, :replies])
-      @search_posts = search_object.uniq.sort_by { |post| post.most_recent_update }.reverse
+    if params[:keyword].blank?
+      @search_posts = Post.all.includes([:user, :replies])
+    else
+      search_objects=[]
+      keywords = params[:keyword].split(/[[:blank:]]+/).select(&:present?)
+      keywords.each do |keyword|
+        posts = Post.search(keyword).includes([:user, :replies])
+        replies = Reply.search(keyword)
+        search_object = posts + Post.where(id: replies.select(:post_id).distinct).includes([:user, :replies])
+        search_objects += search_object
+      end
+      @search_posts = search_objects.uniq.sort_by { |array| array.most_recent_update }.reverse
     end
   end
 end
