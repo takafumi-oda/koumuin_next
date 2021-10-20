32da928fb45d0894db5cad88beb69a98e87c51be
小田崇文 <datti_1988@icloud.com>
2021-10-21 04:58:58 +0900

    検索機能をand検索に変更

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def search
     if params[:keyword].blank?
-      @search_posts = Post.all.includes([:user, :replies])
+      @search_posts = Post.all.includes([:user, :replies]).sort_by { |array| array.most_recent_update }.reverse
     else
       search_objects = []
       keywords = params[:keyword].split(/[[:blank:]]+/).select(&:present?)
@@ -9,9 +9,13 @@ class SearchController < ApplicationController
         posts = Post.search(keyword).includes([:user, :replies])
         replies = Reply.search(keyword)
         search_object = posts + Post.where(id: replies.select(:post_id).distinct).includes([:user, :replies])
-        search_objects += search_object
+        search_objects += search_object.uniq
       end
-      @search_posts = search_objects.uniq.sort_by { |array| array.most_recent_update }.reverse
+      if keywords.length == 1
+        @search_posts = search_objects.sort_by { |array| array.most_recent_update }.reverse
+      else
+        @search_posts = search_objects.group_by{ |e| e }.select { |k, v| v.size > 1 }.map(&:first).sort_by { |array| array.most_recent_update }.reverse
+      end
     end
   end
 end
