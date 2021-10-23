186951618f2efff6771377b7dd9f755e76942b9d
小田崇文 <datti_1988@icloud.com>
2021-10-24 06:08:15 +0900

    相談詳細画面のレイアウト修正

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -17,12 +17,19 @@ class RepliesController < ApplicationController
       else
         flash[:alert] = "この回答への返信が登録できませんでした"
       end
-      redirect_back(fallback_location: root_path)
+      @post = Post.find(@reply.post_id)
+      @replies = @post.replies.includes([:user, :likes])
+      render template: "posts/show"
     end
   end
 
   def destroy
     @reply = Reply.find(params[:id]).destroy
+    unless @reply.parent_id
+      flash[:notice] = "回答を削除しました"
+    else
+      flash[:notice] = "返信を削除しました"
+    end
     redirect_back(fallback_location: root_path)
   end
 
