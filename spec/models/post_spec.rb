b8d4f2f114f19603fd446bb8d27571378a05055b
小田崇文 <datti_1988@icloud.com>
2021-11-01 04:03:27 +0900

    変数の定義を変更

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,24 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  before do
-    @user = create(:test_user)
-    @post = create(:post, user_id: @user.id)
-  end
+  let(:user) { create(:test_user) }
+  let(:post) { create(:post, user_id: user.id) }
 
   describe "バリデーションの検証" do
     it "相談が作成できていること" do
-      expect(@post).to be_valid
+      expect(post).to be_valid
     end
 
     it "タイトルが空では登録できないこと" do
-      @post.title = ''
-      expect(@post).not_to be_valid
+      post.title = ''
+      expect(post).not_to be_valid
     end
 
     it "本文が空では登録できないこと" do
-      @post.content = ''
-      expect(@post).not_to be_valid
+      post.content = ''
+      expect(post).not_to be_valid
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,28 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do
-    @user = create(:test_user)
-  end
+  let(:user) { create(:test_user) }
 
   describe "バリデーションの検証" do
     it "ユーザー登録ができていること" do
-      expect(@user).to be_valid
+      expect(user).to be_valid
     end
 
     it "ユーザー名が空では登録できないこと" do
-      @user.name = ''
-      expect(@user).not_to be_valid
+      user.name = ''
+      expect(user).not_to be_valid
     end
 
     it "ユーザー名が21文字以上では登録できないこと" do
-      @user.name = '012345678901234567890'
-      expect(@user).not_to be_valid
+      user.name = '012345678901234567890'
+      expect(user).not_to be_valid
     end
 
     it "メールアドレスが空では登録できないこと" do
-      @user.email = ''
-      expect(@user).not_to be_valid
+      user.email = ''
+      expect(user).not_to be_valid
     end
 
     it "メールアドレスを重複して登録できないこと" do
@@ -31,8 +29,8 @@ RSpec.describe User, type: :model do
     end
 
     it "メールアドレスがフォーマット（〇〇@〇〇.〇〇）以外では登録できないこと" do
-      @user.email = 'test@email'
-      expect(@user).not_to be_valid
+      user.email = 'test@email'
+      expect(user).not_to be_valid
     end
 
     it "パスワードが空では登録できないこと" do
@@ -46,28 +44,28 @@ RSpec.describe User, type: :model do
     end
 
     it "年代が空では登録できないこと" do
-      @user.age = ''
-      expect(@user).not_to be_valid
+      user.age = ''
+      expect(user).not_to be_valid
     end
 
     it "組織が空では登録できないこと" do
-      @user.organization = ''
-      expect(@user).not_to be_valid
+      user.organization = ''
+      expect(user).not_to be_valid
     end
 
     it "職種が空では登録できないこと" do
-      @user.job = ''
-      expect(@user).not_to be_valid
+      user.job = ''
+      expect(user).not_to be_valid
     end
 
     it "現在の状況が空では登録できないこと" do
-      @user.status = ''
-      expect(@user).not_to be_valid
+      user.status = ''
+      expect(user).not_to be_valid
     end
 
     it "自己紹介が101文字以上では登録できないこと" do
-      @user.introduction = '01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890'
-      expect(@user).not_to be_valid
+      user.introduction = '01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890'
+      expect(user).not_to be_valid
     end
   end
 end
