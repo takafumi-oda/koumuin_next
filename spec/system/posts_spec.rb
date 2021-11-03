e8c23aeede5b9ab3e41a7c3e0e8909522b579bd8
小田崇文 <datti_1988@icloud.com>
2021-11-04 04:28:36 +0900

    ページネーション機能のテストを追加

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -2,48 +2,59 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :system do
   let(:user) { create(:test_user) }
-  let(:post) { create(:post, user_id: user.id) }
+  let!(:posts) { create_list(:post, 10, user_id: user.id)}
+  let!(:post) { create(:post, title: "テスト相談11", user_id: user.id) }
 
-  before do
-    visit login_path
-    fill_in "メールアドレス", with: user.email
-    fill_in "パスワード", with: user.password
-    click_on "ログインする"
+  describe "ページネーション機能の検証", js: true do
+    it "ページネーション機能が動いていること" do
+      visit root_path
+      click_link "2"
+      expect(page).to have_content post.title
+    end
   end
 
-  describe "相談の新規登録" do
+  describe "ログイン後の相談機能の検証" do
     before do
-      visit new_post_path
+      visit login_path
+      fill_in "メールアドレス", with: user.email
+      fill_in "パスワード", with: user.password
+      click_on "ログインする"
     end
 
-    it "登録成功" do
-      fill_in "タイトル", with: "テスト相談のタイトル"
-      fill_in "本文", with: "テスト相談の本文"
-      click_button "相談する"
-      expect(page).to have_content "新しい相談をしました"
-    end
+    describe "相談の新規登録" do
+      before do
+        visit new_post_path
+      end
 
-    it "登録失敗" do
-      click_button "相談する"
-      expect(page).to have_content "新しい相談の登録に失敗しました"
-    end
-  end
+      it "登録成功" do
+        fill_in "タイトル", with: "テスト相談のタイトル"
+        fill_in "本文", with: "テスト相談の本文"
+        click_button "相談する"
+        expect(page).to have_content "新しい相談をしました"
+      end
 
-  describe "相談の削除" do
-    describe "回答がない場合" do
-      it "削除成功" do
-        visit post_path(post.id)
-        click_on "相談を削除する"
-        expect(page).to have_content "相談を削除しました"
+      it "登録失敗" do
+        click_button "相談する"
+        expect(page).to have_content "新しい相談の登録に失敗しました"
       end
     end
 
-    describe "回答がある場合" do
-      let!(:reply) { create(:reply, user_id: user.id, post_id: post.id) }
+    describe "相談の削除" do
+      describe "回答がない場合" do
+        it "削除成功" do
+          visit post_path(post.id)
+          click_on "相談を削除する"
+          expect(page).to have_content "相談を削除しました"
+        end
+      end
 
-      it "削除不可" do
-        visit post_path(post.id)
-        expect(page).not_to have_content "相談を削除する"
+      describe "回答がある場合" do
+        let!(:reply) { create(:reply, user_id: user.id, post_id: post.id) }
+
+        it "削除不可" do
+          visit post_path(post.id)
+          expect(page).not_to have_content "相談を削除する"
+        end
       end
     end
   end
