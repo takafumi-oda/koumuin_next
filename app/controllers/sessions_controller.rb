913050d7cc14380225594afe2ed1ad2cd289b72f
takafumi-oda <datti_1988@icloud.com>
2021-11-26 04:46:23 +0900

    フラッシュメッセージに関するミスを修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path, notice: "ログインしました"
     else
-      flash.now[:alert] = "メールアドレスかパスワードが間違っています"
+      flash[:alert] = "メールアドレスかパスワードが間違っています"
       redirect_back(fallback_location: root_path)
     end
   end
